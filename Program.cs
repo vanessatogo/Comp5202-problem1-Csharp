424e867ce11c8f1f07044e3857f017da0d53da34
Jacob Van Silfhout <jv1@bcs.net.nz>
2017-09-20 10:01:39 +1200

    9960471-problem01-csharp

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _9960471_problem01_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Hello this programe will ask for two number input and show which of the two numbers are higher ");
+        //tell user what the programme does
+        Console.ReadLine();
+        Console.WriteLine("Please enter your first number");//ask user for input
+           var num1 = Console.ReadLine();
+        Console.WriteLine("Please enter your second number");
+           var num2 = Console.ReadLine();
+            if(num1<num2);//calculate which of the 2 are higher
+            {
+        Console.WriteLine("Your first number is higher than you second number");
+           }
+
+           else(num2<num1);
+           
+            {
+            //out put the answer
+        Console.WriteLine("Your second number is higher than your first number");
+            }
+        }
+    }
+}
